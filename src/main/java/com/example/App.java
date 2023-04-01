a4fad753f1b1ea5ab1e0b844c2fb79062cf3a321
Alfonso Segura <a.segura.gonzalez@gmail.com>
2023-04-01 12:56:30 +0000

    chore: configure checkstyle

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,22 @@
 package com.example;
 
 /**
- * Hello world!
+ * Application component.
  *
+ * @author John Doe
+ * @version 1.0.0
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public final class App {
+
+    private App() {
+    }
+
+    /**
+     * Application entry point.
+     *
+     * @param args Arguments array received from CLI call.
+     */
+    public static void main(final String[] args) {
+        System.out.println("hello World!");
     }
 }
